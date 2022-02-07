0343b7a6bee048cd22e6db19eeaaf087ab02c7c6
Cynthia <cynthia.konar@gmail.com>
2022-02-07 20:02:46 +0530

    commit on 'qr-scan-v1'

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-//import 'history.dart';
+import 'screens/home/home.dart';
+import 'screens/home/history.dart';
+import 'screens/authenticate/login.dart';
+import 'screens/authenticate/signup.dart';
 
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomePage(),
-        //home: HistoryPage(),
+        routes: {
+          '/home': (context) => HomePage(),
+          '/history': (context) => HistoryPage(),
+          '/signup': (context) => SignUpPage(),
+          '/login': (context) => LoginPage(),
+        },
       ),
     );
